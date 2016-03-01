5cc6b22982724a00575034dddee6c0de4849406b
theodoredottavio <teddottavio@yahoo.com>
2016-03-01 00:45:59 -0500

    setup the download

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,10 @@
 class HomepageController < ApplicationController
   def index
   end
+  
+  def downloadpc
+    send_file 'app/product/xrobotattack.zip', filename: 'xrobotattack.zip',
+       type: 'application/zip',
+       disposition: 'attachment'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
    match '/home', to: 'homepage#index', via: 'get'
+   match '/pc', to: 'homepage#downloadpc', via: 'get'
    root 'homepage#index'
 
    #resources :scores
    match '/score', to: 'highscores#index', via: 'get'
-   #match '/score', to: 'highscores#gamereader', via: 'get'
 end
