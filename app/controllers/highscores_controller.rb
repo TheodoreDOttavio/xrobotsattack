8ad3c569e81417c47a36daed570742737c39ac29
theodoredottavio <teddottavio@yahoo.com>
2016-03-01 03:19:19 -0500

    adjusted the highscore data to match the app

--- app/controllers/highscores_controller.rb
+++ app/controllers/highscores_controller.rb
@@ -2,13 +2,13 @@ class HighscoresController < ApplicationController
  
   def index
     if params[:score].nil? != true then
-      if params[:player].length >= 2 and params[:score] != "0" then
+      if params[:player].length >= 3 and params[:score] != "0" then
         cleanedparams = {:score => params[:score].to_i, :player => params[:player] }
         @score = Score.new(cleanedparams)
         @score.save
       end
     end
-    @showscores = Score.all
+    @showscores = Score.top
   end
 
 end
--- app/models/score.rb
+++ app/models/score.rb
@@ -1,2 +1,3 @@
 class Score < ActiveRecord::Base
+  scope :top, -> { order(score: :desc).limit(24) }
 end
