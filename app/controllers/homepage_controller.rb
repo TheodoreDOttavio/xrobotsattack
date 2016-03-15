bc57b5de2e16a2b7d460a1c68ab894131e5cfd86
theodoredottavio <teddottavio@yahoo.com>
2016-03-15 15:14:45 -0400

    compressed graphics into exe and did away with the zipfile

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -3,8 +3,11 @@ class HomepageController < ApplicationController
   end
   
   def downloadpc
-    send_file 'app/product/xrobotsattack.zip', filename: 'xrobotsattack.zip',
-       type: 'application/zip',
+    #send_file 'app/product/xrobotsattack.zip', filename: 'xrobotsattack.zip',
+     #  type: 'application/zip',
+       
+    send_file 'app/product/xrobots.exe', filename: 'xrobots.exe',
+       type: 'application/exe',
        disposition: 'attachment'
   end
 end
